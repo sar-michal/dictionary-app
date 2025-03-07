f2e2e5d61bc6b1f06c4558cb74fa982fe8d820f9
Michał Sarosiek <sarmichal@yahoo.com>
2025-03-07 23:20:49 +0100

    Add cosmetic changes to postgres.go

--- storage/postgres.go
+++ storage/postgres.go
@@ -2,13 +2,14 @@ package storage
 
 import (
 	"fmt"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 type Config struct {
 	Host     string
-	Port	 string
+	Port     string
 	User     string
 	Password string
 	DBName   string
@@ -18,9 +19,9 @@ type Config struct {
 func NewConnection(config *Config) (*gorm.DB, error) {
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-		config.Host, config.User, config.Password, config.DBName, config.Port, config.SSLMode
+		config.Host, config.User, config.Password, config.DBName, config.Port, config.SSLMode,
 	)
-	
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
