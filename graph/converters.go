3dfe4293cf110fb91d5a3267c50210e9a9876257
Michał Sarosiek <sarmichal@yahoo.com>
2025-03-17 23:35:12 +0100

    Add converters for singular objects

--- graph/converters.go
+++ graph/converters.go
@@ -8,11 +8,21 @@ import (
 )
 
 // Convert models Word to a GraphQL Word
-func convertWord(w *models.Word) *model.Word {
+func convertWord(word *models.Word) *model.Word {
 	return &model.Word{
-		WordID:       strconv.FormatUint(uint64(w.WordID), 10),
-		PolishWord:   w.PolishWord,
-		Translations: convertTranslations(w.Translations),
+		WordID:       strconv.FormatUint(uint64(word.WordID), 10),
+		PolishWord:   word.PolishWord,
+		Translations: convertTranslations(word.Translations),
+	}
+}
+
+// Convert a single models Translation to a GraphQL Translation
+func convertTranslation(translation *models.Translation) *model.Translation {
+	return &model.Translation{
+		TranslationID:      strconv.FormatUint(uint64(translation.TranslationID), 10),
+		EnglishTranslation: translation.EnglishTranslation,
+		WordID:             strconv.FormatUint(uint64(translation.WordID), 10),
+		ExampleSentences:   convertExampleSentences(translation.ExampleSentences),
 	}
 }
 
@@ -20,25 +30,25 @@ func convertWord(w *models.Word) *model.Word {
 func convertTranslations(translations []models.Translation) []*model.Translation {
 	gqlTranslations := make([]*model.Translation, len(translations))
 	for i, t := range translations {
-		gqlTranslations[i] = &model.Translation{
-			TranslationID:      strconv.FormatUint(uint64(t.TranslationID), 10),
-			EnglishTranslation: t.EnglishTranslation,
-			WordID:             strconv.FormatUint(uint64(t.WordID), 10),
-			ExampleSentences:   convertExampleSentences(t.ExampleSentences),
-		}
+		gqlTranslations[i] = convertTranslation(&t)
 	}
 	return gqlTranslations
+}
+
+// Convert a single models ExampleSentence to a GraphQL ExampleSentence
+func convertExampleSentence(sentence *models.ExampleSentence) *model.ExampleSentence {
+	return &model.ExampleSentence{
+		SentenceID:    strconv.FormatUint(uint64(sentence.SentenceID), 10),
+		SentenceText:  sentence.SentenceText,
+		TranslationID: strconv.FormatUint(uint64(sentence.TranslationID), 10),
+	}
 }
 
 // Convert a slice of models ExampleSentence to GraphQL ExampleSentence
 func convertExampleSentences(sentences []models.ExampleSentence) []*model.ExampleSentence {
 	gqlSentences := make([]*model.ExampleSentence, len(sentences))
 	for i, s := range sentences {
-		gqlSentences[i] = &model.ExampleSentence{
-			SentenceID:    strconv.FormatUint(uint64(s.SentenceID), 10),
-			SentenceText:  s.SentenceText,
-			TranslationID: strconv.FormatUint(uint64(s.TranslationID), 10),
-		}
+		gqlSentences[i] = convertExampleSentence(&s)
 	}
 	return gqlSentences
 }
