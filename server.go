63475611c05ae2ced136406011f263dcdf8250c8
Michał Sarosiek <sarmichal@yahoo.com>
2025-03-17 20:36:36 +0100

    Integrate repository with server

--- server.go
+++ server.go
@@ -11,10 +11,14 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/sar-michal/dictionary-app/graph"
+	"github.com/sar-michal/dictionary-app/pkg/config"
+	"github.com/sar-michal/dictionary-app/pkg/models"
+	"github.com/sar-michal/dictionary-app/pkg/repository"
+	"github.com/sar-michal/dictionary-app/pkg/storage"
 	"github.com/vektah/gqlparser/v2/ast"
 )
 
-const defaultPort = "8080"
+const defaultPort = "8082"
 
 func main() {
 	port := os.Getenv("PORT")
@@ -22,7 +26,30 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	os.Setenv("GO_ENV", "development")
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	db, err := storage.NewConnection(cfg)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer func() {
+		if err := storage.CloseDB(db); err != nil {
+			log.Printf("Error closing database: %v", err)
+		}
+	}()
+
+	if err := models.Migrate(db); err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
+	}
+
+	repo := &repository.GormRepository{DB: db}
+
+	resolver := &graph.Resolver{Repo: repo}
+	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: resolver}))
 
 	srv.AddTransport(transport.Options{})
 	srv.AddTransport(transport.GET{})
